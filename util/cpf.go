249dcccef112fd1e8d7a1dbee1d63f2dca1bd631
Igor Melo <igoracmelo@protonmail.com>
2023-03-03 01:12:24 -0300

    feat: validação de CPF

--- util/cpf.go
+++ util/cpf.go
@@ -1,8 +1,12 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-// TODO:
+// `ValidateCpfOrCnpj` espera uma string `val` já sanitizada e verifica se
+// `val` representa um CPF ou CNPJ matematicamente válido
 func ValidateCpfOrCnpj(val string) error {
 	if len(val) == 11 {
 		return ValidateCpf(val)
@@ -14,9 +18,50 @@ func ValidateCpfOrCnpj(val string) error {
 }
 
 func ValidateCpf(val string) error {
+	sum := 0
+
+	// Somando os 9 primeiros dígitos do CPF, aplicando os seguintes pesos:
+	// (A * 10) + (B * 9) + (C * 8) + (D * 7) + (E * 6) + (F * 5) + (G * 4) + (H * 3) + (I * 2)
+	// onde as letras repesentariam um CPF ABC.DEF.GHI-JK
+	for i := 0; i < 9; i++ {
+		digit, err := strconv.Atoi(val[i : i+1])
+		if err != nil {
+			return err
+		}
+		sum += (10 - i) * digit
+	}
+
+	wantDigit10 := strconv.Itoa(sum * 10 % 11 % 10) // dígito esperado
+	gotDigit10 := val[9:10]                         // dígito obtido
+
+	if wantDigit10 != gotDigit10 {
+		return fmt.Errorf("expected digit at position 10 to be %s, but got %s", wantDigit10, gotDigit10)
+	}
+
+	sum = 0
+
+	// Somando os 10 primeiros dígitos do CPF, aplicando os seguintes pesos:
+	// (A * 11) + (B * 10) + (C * 9) + (D * 8) + (E * 7) + (F * 6) + (G * 5) + (H * 4) + (I * 3) + (J * 2)
+	// onde as letras repesentariam um CPF ABC.DEF.GHI-JK
+	for i := 0; i < 10; i++ {
+		digit, err := strconv.Atoi(val[i : i+1])
+		if err != nil {
+			return err
+		}
+		sum += (11 - i) * digit
+	}
+
+	wantDigit11 := strconv.Itoa(sum * 10 % 11 % 10) // dígito esperado
+	gotDigit11 := val[10:11]                        // dígito obtido
+
+	if wantDigit11 != gotDigit11 {
+		return fmt.Errorf("expected digit at position 11 to be %s, but got %s", wantDigit11, gotDigit11)
+	}
+
 	return nil
 }
 
+// TODO:
 func ValidateCnpj(val string) error {
 	return nil
 }
--- util/cpf_test.go
+++ util/cpf_test.go
@@ -12,8 +12,14 @@ func Test_ValidateCpfOrCnpj(t *testing.T) {
 		{val: "0123456789", wantErr: true},
 		{val: "012345678910", wantErr: true},
 		{val: "0123456789101213", wantErr: true},
+
 		{val: "66849734008", wantErr: false},
-		{val: "66849734000", wantErr: true},
+		{val: "66849734018", wantErr: true}, // alterar dígito 10 deve causar erro
+		{val: "66849734005", wantErr: true}, // alterar dígito 11 deve causar erro
+
+		{val: "45091647007", wantErr: false},
+		{val: "45291647007", wantErr: true}, // alterar qualquer dígito deve causar erro
+		{val: "45091607007", wantErr: true}, // alterar qualquer dígito deve causar erro
 	}
 
 	for _, tt := range tests {
