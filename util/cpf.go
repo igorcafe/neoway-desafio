afb1a5b50e73cc779b4501a376f58e7a26dcc566
Igor Melo <igoracmelo@protonmail.com>
2023-03-04 22:58:38 -0300

    simplificando validação de CPF

--- util/cpf.go
+++ util/cpf.go
@@ -41,18 +41,19 @@ func ValidateCpf(val string) error {
 	// (A * 10) + (B * 9) + (C * 8) + (D * 7) + (E * 6) + (F * 5) + (G * 4) + (H * 3) + (I * 2)
 	// onde as letras repesentariam um CPF ABC.DEF.GHI-JK
 	for i := 0; i < 9; i++ {
-		digit, err := strconv.Atoi(val[i : i+1])
-		if err != nil {
-			return err
-		}
+
+		// converte o dígito em ascii para o número en inteiro
+		// valores inválidos, como letras e símbolos não são tratados aqui,
+		// mas sim na sanitização
+		digit := int(val[i] - '0')
 		sum += (10 - i) * digit
 	}
 
-	wantDigit10 := strconv.Itoa(sum * 10 % 11 % 10) // dígito esperado
-	gotDigit10 := val[9:10]                         // dígito obtido
+	wantDigit10 := sum * 10 % 11 % 10 // dígito esperado
+	gotDigit10 := int(val[9] - '0')   // dígito obtido
 
 	if wantDigit10 != gotDigit10 {
-		return fmt.Errorf("expected digit at position 10 to be %s, but got %s", wantDigit10, gotDigit10)
+		return fmt.Errorf("expected digit at position 10 to be %d, but got %d", wantDigit10, gotDigit10)
 	}
 
 	sum = 0
@@ -61,24 +62,20 @@ func ValidateCpf(val string) error {
 	// (A * 11) + (B * 10) + (C * 9) + (D * 8) + (E * 7) + (F * 6) + (G * 5) + (H * 4) + (I * 3) + (J * 2)
 	// onde as letras repesentariam um CPF ABC.DEF.GHI-JK
 	for i := 0; i < 10; i++ {
-		digit, err := strconv.Atoi(val[i : i+1])
-		if err != nil {
-			return err
-		}
+		digit := int(val[i] - '0')
 		sum += (11 - i) * digit
 	}
 
-	wantDigit11 := strconv.Itoa(sum * 10 % 11 % 10) // dígito esperado
-	gotDigit11 := val[10:11]                        // dígito obtido
+	wantDigit11 := sum * 10 % 11 % 10 // dígito esperado
+	gotDigit11 := int(val[10] - '0')  // dígito obtido
 
 	if wantDigit11 != gotDigit11 {
-		return fmt.Errorf("expected digit at position 11 to be %s, but got %s", wantDigit11, gotDigit11)
+		return fmt.Errorf("expected digit at position 11 to be %d, but got %d", wantDigit11, gotDigit11)
 	}
 
 	return nil
 }
 
-// TODO:
 func ValidateCnpj(val string) error {
 	return nil
 }
